28e1f5155c07f1bba7e00a388d31a560a70210db
Jakefil <kofidiligent00@gmail.com>
2023-02-16 15:50:56 -0800

    5-printf.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main A program that print a line with puts function.
+ * Return:0 (success)
+ */
+int main(void)
+{
+/** Description:The program's description prints aline */
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
