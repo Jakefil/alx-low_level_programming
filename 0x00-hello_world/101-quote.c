0614cb5785423e41dd78221c4c4f0fda1f9c4e1b
Jakefil <kofidiligent00@gmail.com>
2023-02-16 17:01:19 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-int fput;
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
