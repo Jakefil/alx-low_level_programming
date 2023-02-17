e5dc8e23d087e4d04b7b1b6d0eeb2b5575534828
Jakefil <kofidiligent00@gmail.com>
2023-02-17 13:54:32 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - function
+ * Return: (0)
+ */
+int main(void)
+{
+int x = 97, y = 122;
+while (x <= y)
+putchar{
+	putchar(x);
+	x++;
+}
+putchar("\n");
+return (0);
+}
