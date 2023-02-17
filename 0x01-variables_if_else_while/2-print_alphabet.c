afe35b6b42cd9d9747f1113aafa82e4d6fc4ca83
Jakefil <kofidiligent00@gmail.com>
2023-02-17 14:14:32 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-char x;
-for (x = 'a'; x <= 'Z'; x++)
-	putchar(x);
+char letter;
+for (letter = 'a'; letter <= 'Z'; letter++)
+	putchar(letter);
 putchar("\n");
 return (0);
 }
